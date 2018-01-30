b4e27321a95c3f2c481af2d68558bbf17f435a49
voaignidc <1171595505@qq.com>
2018-01-30 22:25:38 +0800

    move to static folder

---
+++ js/cnvs.js
@@ -0,0 +1,39 @@
+function fakeClick(obj)
+{  
+  var ev = document.createEvent("MouseEvents");  
+  ev.initMouseEvent(  
+      "click", true, false, window, 0, 0, 0, 0, 0  
+      , false, false, false, false, 0, null  
+      );  
+  obj.dispatchEvent(ev);  
+} 
+
+function savaFile(data, filename)
+{
+    var saveLink = document.createElement('a');
+    saveLink.href = data;
+    saveLink.download = filename;
+    fakeClick(saveLink); 
+}
+
+function inputFileButtonChecked()
+{
+  //大括号不换行,我浑身难受!
+  var fileInput = document.getElementById("inputFileId").files[0];
+  var reader = new FileReader(); 
+  reader.readAsDataURL(fileInput);//将文件以Data URL形式读入页面  
+
+  reader.onload = function()
+  { 
+    var c = document.getElementById("srcImageCanvas"); 
+    var cxt = c.getContext("2d");
+    var img = new Image();
+    
+    img.onload = function()
+    {
+      cxt.drawImage(img,0,0);
+    }
+    img.src = reader.result;
+    savaFile(reader.result, "aaa.bmp");//将图片保存到本地
+  }   
+}
--- start.py
+++ start.py
@@ -3,10 +3,21 @@
 import os
 import sys
 import webbrowser
+from flask import Flask
 
 if getattr(sys, 'frozen', False):
     rootPath = os.path.dirname(sys.executable)
 elif __file__:
     rootPath = os.path.dirname(__file__)    
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return app.send_static_file('base.html')
     
-webbrowser.open(rootPath+"/templates/base.html")
+if __name__ == '__main__':
+    #webbrowser.open(rootPath+"/templates/base.html")
+    webbrowser.open("http://127.0.0.1:5000/")
+    app.debug = True
+    app.run()
