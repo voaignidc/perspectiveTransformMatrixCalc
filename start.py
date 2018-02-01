83df4514172616379fedc500b75f73624ff8ad39
voaignidc <1171595505@qq.com>
2018-02-01 18:36:33 +0800

    fix bug that cant find css&js

--- start.py
+++ start.py
@@ -3,7 +3,13 @@
 import os
 import sys
 import webbrowser
-from flask import Flask
+
+
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 if getattr(sys, 'frozen', False):
     rootPath = os.path.dirname(sys.executable)
@@ -11,10 +17,24 @@ elif __file__:
     rootPath = os.path.dirname(__file__)    
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard_to_guess_string'
+
+
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
-    return app.send_static_file('base.html')
+    # name = None
+    # form = NameForm()
+    # if form.validate_on_submit():
+        # name = form.name.data
+        # form.name.data = ''
+    return render_template('base.html')#, form=form, name=name)
+    
     
 if __name__ == '__main__':
     #webbrowser.open(rootPath+"/templates/base.html")
